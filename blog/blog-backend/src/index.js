58552b496db21854c19414d37d4dc2243b7bc041
GeekCoderX <closeplz0526@gmail.com>
2025-03-16 09:17:55 +0900

    blog-backend update

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,16 +1,36 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
-router.use("/api", api.routes());
+// 라우터 설정
+router.use('/api', api.routes());
 
+// 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+// app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('listening port is 4000');
+// PORT가 지정되어 있지 않다면 4000을 적용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
